d491f0639bbe2102e48f803d1fc4ad93204fe7da
Big One <sponjjanc@naver.com>
2023-09-06 12:43:36 +0900

    feat: exceptional handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,22 @@ import styles from "./page.module.css";
 import React from "react";
 import path from "path";
 import fs from "fs";
+import { notFound, redirect } from "next/navigation";
 
 async function getProducts() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonFile = await fs.readFileSync(filePath);
-  return JSON.parse(jsonFile as string);
+  const data = JSON.parse(jsonFile as string);
+  if (!data) return redirect("/no-data");
+  if (data.products.length === 0) return notFound();
+
+  return data;
 }
 
 export default async function HomePage() {
   const { products } = await getProducts();
   console.log(products);
+
   return (
     <main className={styles.main}>
       <ul>
